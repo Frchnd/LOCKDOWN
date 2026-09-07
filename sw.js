b51817979b79bd81d43c6ada249d932f6f7ee6dd
Fernando Chandra <fernandochandra2001@gmail.com>
2026-09-07 14:57:32 +0700

    v0.19.6

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='lockdown-v19-5';
+const CACHE='lockdown-v19-6';
 const CORE=[
- './','./index.html','./app.css?v=0195','./app.js?v=0195','./manifest.webmanifest',
+ './','./index.html','./app.css?v=0196','./app.js?v=0196','./manifest.webmanifest',
  './assets/menu-bg.webp','./assets/icon-192.png','./assets/icon-512.png','./assets/concept-board.webp',
  './assets/prologue/prologue_01_emergency.webp',
  './assets/prologue/prologue_02_flash.webp',
